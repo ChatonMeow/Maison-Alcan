f945c71a0d009f0c4d0e914fa6e8533ec1f412f0
David Malo <david@leeroy.ca>
2021-09-27 22:22:34 -0400

    Activer locoscroll

--- wp-content/themes/maisonalcan/app/setup.php
+++ wp-content/themes/maisonalcan/app/setup.php
@@ -13,8 +13,8 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('aktiv-grotesk', 'https://use.typekit.net/ouz7pur.css', false, null);
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    //wp_enqueue_style('LocomotiveScrollCSS', 'https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.css');
-    //wp_enqueue_script('LocomotiveScroll', 'https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.js', [], null, true);
+    wp_enqueue_style('LocomotiveScrollCSS', 'https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.css');
+    wp_enqueue_script('LocomotiveScroll', 'https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.js', [], null, true);
     wp_enqueue_style('SwiperCSS', 'https://unpkg.com/swiper@7/swiper-bundle.min.css');
     wp_enqueue_script('Swiper', 'https://unpkg.com/swiper@7/swiper-bundle.min.js', [], null, true);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'/*, 'LocomotiveScroll'*/], null, true);
--- wp-content/themes/maisonalcan/resources/assets/scripts/routes/common.js
+++ wp-content/themes/maisonalcan/resources/assets/scripts/routes/common.js
@@ -1,21 +1,26 @@
 export default {
   init() {
     // JavaScript to be fired on all pages;
+  },
+  finalize() {
+    // JavaScript to be fired on all pages, after page specific JS is fired
 
     //Locomotive Scroll
     // eslint-disable-next-line no-unused-vars, no-undef
-    /*const scroll = new LocomotiveScroll({
+    const scroll = new LocomotiveScroll({
       el: document.querySelector('[data-scroll-container]'),
       smooth: true,
-    });*/
+    });
+
+    //Besoin de la ligne suivante pour que le scroll soit mesuré correctement
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 2000);
 
     //Nav
     document.querySelector('.header .menu-toggle').addEventListener('click', () => {
       document.querySelector('html').classList.toggle('nav-open');
       window.scrollTo(0, 0);
-    })
-  },
-  finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
+    });
   },
 };
